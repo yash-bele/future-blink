8410010b1324cdc2345788fe108b67ee9ef331dc
Yash Bele <yashgbele@gmail.com>
2024-05-09 14:51:35 +0530

    Update Login.jsx

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
             spellCheck="false"
             value={inputs.password}
             onChange={handleChange("password")}
-            type="text"
+            type="password"
             id="password"
             className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             placeholder="Enter your password"
